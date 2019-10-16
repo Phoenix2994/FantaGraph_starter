fc2c79e004e450e826f4d3eacff9bf104475100f
Phoenix2994 <filippobianco2@gmail.com>
2019-10-16 19:17:19 +0200

    fix and composite indices added

--- src/main/java/fantagraph/graph/build/FantaGraphBuild.java
+++ src/main/java/fantagraph/graph/build/FantaGraphBuild.java
@@ -28,18 +28,20 @@ public class FantaGraphBuild {
         management.makeVertexLabel("presidente").make();
         management.makeVertexLabel("squadra").make();
         management.makeVertexLabel("stadio").make();
+        management.makeVertexLabel("campionato").make();
     }
 
     /**
      * Creates the edge labels.
      */
     private static void createEdgeLabels(final JanusGraphManagement management) {
-        management.makeEdgeLabel("gioca_per").make();
-        management.makeEdgeLabel("gioca_in").make();
+        management.makeEdgeLabel("gioca per").make();
+        management.makeEdgeLabel("gioca in").make();
         management.makeEdgeLabel("allena").make();
-        management.makeEdgeLabel("è_assistito_da").make();
+        management.makeEdgeLabel("è assistito da").make();
         management.makeEdgeLabel("possiede").make();
-        management.makeEdgeLabel("ha_assunto").make();
+        management.makeEdgeLabel("è incaricato da").make();
+        management.makeEdgeLabel("partecipa a").make();
     }
 
     /**
@@ -47,9 +49,16 @@ public class FantaGraphBuild {
      */
     private static void createProperties(final JanusGraphManagement management) {
         management.makePropertyKey("nome").dataType(String.class).make();
-        management.makePropertyKey("data_nascita").dataType(String.class).make();
-        management.makePropertyKey("luogo_nascita").dataType(String.class).make();
-        management.makePropertyKey("media_eta").dataType(Double.class).make();
+        management.makePropertyKey("nome squadra").dataType(String.class).make();
+        management.makePropertyKey("nome giocatore").dataType(String.class).make();
+        management.makePropertyKey("nome presidente").dataType(String.class).make();
+        management.makePropertyKey("nome allenatore").dataType(String.class).make();
+        management.makePropertyKey("nome stadio").dataType(String.class).make();
+        management.makePropertyKey("nome procuratore").dataType(String.class).make();
+        management.makePropertyKey("nome campionato").dataType(String.class).make();
+        management.makePropertyKey("data nascita").dataType(String.class).make();
+        management.makePropertyKey("luogo nascita").dataType(String.class).make();
+        management.makePropertyKey("media eta").dataType(Double.class).make();
         management.makePropertyKey("rosa").dataType(Long.class).make();
         management.makePropertyKey("citta").dataType(String.class).make();
         management.makePropertyKey("capienza").dataType(Long.class).make();
@@ -61,6 +70,7 @@ public class FantaGraphBuild {
         management.makePropertyKey("statistiche").dataType(String.class).make();
         management.makePropertyKey("img").dataType(String.class).make();
         management.makePropertyKey("id").dataType(String.class).make();
+        management.makePropertyKey("paese").dataType(String.class).make();
     }
 
     private static void createSchema(final JanusGraphManagement management) {
@@ -72,7 +82,7 @@ public class FantaGraphBuild {
         management.commit();
     }
 
-    private static void createPlayerVertex(GraphTraversalSource g) {
+    private static void createPlayersVertices(GraphTraversalSource g) {
         JSONParser parser = new JSONParser();
 
         try {
@@ -93,20 +103,20 @@ public class FantaGraphBuild {
                 String player_id = (String) player_info.get("id");
                 String player_stats = player_info.get("statistiche").toString();
                 String prosecutor_name = (String) player_info.get("procuratore");
-                final Vertex player = g.addV("giocatore").property("nome", name).property("nome", name).property("data_nascita", player_data).
-                        property("luogo_nascita", player_place).property("nazionalita", player_nat).property("altezza", player_height).
+                final Vertex player = g.addV("giocatore").property("nome giocatore", name).property("data nascita", player_data).
+                        property("luogo nascita", player_place).property("nazionalita", player_nat).property("altezza", player_height).
                         property("ruolo", player_role).property("piede", player_foot).property("img", player_img).
                         property("id", player_id).property("statistiche", player_stats).next();
-                Vertex team = g.V().hasLabel("squadra").has("nome", (team_name + " ").split(" ")[0].toUpperCase()).next();
-                g.V(player).as("a").V(team).addE("gioca_per").from("a").next();
-                boolean exist = g.V().hasLabel("procuratore").has("nome", prosecutor_name).hasNext();
+                Vertex team = g.V().hasLabel("squadra").has("nome squadra", (team_name + " ").split(" ")[0].toUpperCase()).next();
+                g.V(player).as("a").V(team).addE("gioca per").from("a").next();
+                boolean exist = g.V().hasLabel("procuratore").has("nome procuratore", prosecutor_name).hasNext();
                 final Vertex prosecutor;
                 if (!exist) {
-                    prosecutor = g.addV("procuratore").property("nome", prosecutor_name).next();
+                    prosecutor = g.addV("procuratore").property("nome procuratore", prosecutor_name).next();
                 } else {
-                    prosecutor = g.V().hasLabel("procuratore").has("nome", prosecutor_name).next();
+                    prosecutor = g.V().hasLabel("procuratore").has("nome procuratore", prosecutor_name).next();
                 }
-                g.V(player).as("a").V(prosecutor).addE("è_assistito_da").from("a").next();
+                g.V(player).as("a").V(prosecutor).addE("è assistito da").from("a").next();
                 g.tx().commit();
             }
         } catch (Exception e) {
@@ -115,7 +125,7 @@ public class FantaGraphBuild {
         }
     }
 
-    private static void createTeamVertex(GraphTraversalSource g) {
+    private static void createInfosVertices(GraphTraversalSource g) {
         JSONParser parser = new JSONParser();
         try {
             FantaGraphBuild main = new FantaGraphBuild();
@@ -127,18 +137,25 @@ public class FantaGraphBuild {
                 JSONObject team_info = (JSONObject) jsonObject.get(key);
                 Double avg_age = (Double) team_info.get("media eta");
                 Long team_members = (Long) team_info.get("rosa");
-                final Vertex team = g.addV("squadra").property("nome", name).property("rosa", team_members).property("media eta", avg_age).next();
+                final Vertex team = g.addV("squadra").property("nome squadra", name).property("rosa", team_members).property("media eta", avg_age).next();
                 //team vertex added to graph
                 JSONObject stadium_info = (JSONObject) team_info.get("stadio");
                 String stadium_name = (String) stadium_info.get("nome");
                 String stadium_place = (String) stadium_info.get("citta");
                 Long stadium_fans = (Long) stadium_info.get("capienza");
-                boolean exist = g.V().hasLabel("stadio").has("nome", stadium_name).hasNext();
+                boolean stadium_exist = g.V().hasLabel("stadio").has("nome stadio", stadium_name).hasNext();
                 final Vertex stadium;
-                if (!exist) {
-                    stadium = g.addV("stadio").property("nome", stadium_name).property("citta", stadium_place).property("capienza", stadium_fans).next();
+                if (!stadium_exist) {
+                    stadium = g.addV("stadio").property("nome stadio", stadium_name).property("citta", stadium_place).property("capienza", stadium_fans).next();
                 } else {
-                    stadium = g.V().hasLabel("stadio").has("nome", stadium_name).next();
+                    stadium = g.V().hasLabel("stadio").has("nome stadio", stadium_name).next();
+                }
+                boolean league_exist = g.V().hasLabel("campionato").has("nome campionato", "Serie A TIM").hasNext();
+                final Vertex league;
+                if (!league_exist) {
+                    league = g.addV("campionato").property("nome campionato", "Serie A TIM").property("paese", "ITALIA").next();
+                } else {
+                    league = g.V().hasLabel("campionato").has("nome campionato", "Serie A TIM").next();
                 }
                 //stadium vertex added to graph
                 JSONObject president_info = (JSONObject) team_info.get("presidente");
@@ -146,7 +163,7 @@ public class FantaGraphBuild {
                 String pres_place = (String) president_info.get("luogo nascita");
                 String pres_data = (String) president_info.get("data nascita");
                 String pres_nat = (String) president_info.get("nazionalita");
-                final Vertex president = g.addV("presidente").property("nome", pres_name).property("data_nascita", pres_data).property("luogo_nascita", pres_place).property("nazionalita", pres_nat).next();
+                final Vertex president = g.addV("presidente").property("nome presidente", pres_name).property("data nascita", pres_data).property("luogo nascita", pres_place).property("nazionalita", pres_nat).next();
                 //president vertex added to graph
                 JSONObject coach_info = (JSONObject) team_info.get("allenatore");
                 String coach_name = (String) coach_info.get("nome");
@@ -154,11 +171,12 @@ public class FantaGraphBuild {
                 String coach_data = (String) coach_info.get("data nascita");
                 String coach_nat = (String) coach_info.get("nazionalita");
                 Long coach_schema = (Long) coach_info.get("modulo");
-                final Vertex coach = g.addV("allenatore").property("nome", coach_name).property("data_nascita", coach_data).property("luogo_nascita", coach_place).property("nazionalita", coach_nat).property("modulo", coach_schema).next();
+                final Vertex coach = g.addV("allenatore").property("nome allenatore", coach_name).property("data nascita", coach_data).property("luogo nascita", coach_place).property("nazionalita", coach_nat).property("modulo", coach_schema).next();
                 //coach vertex added to graph
-                g.V(team).as("a").V(stadium).addE("gioca_in").from("a").next();
+                g.V(team).as("a").V(stadium).addE("gioca in").from("a").next();
+                g.V(team).as("a").V(league).addE("partecipa a").from("a").next();
                 g.V(coach).as("a").V(team).addE("allena").from("a").next();
-                g.V(president).as("a").V(coach).addE("ha_assunto").from("a").next();
+                g.V(coach).as("a").V(president).addE("è incaricato da").from("a").next();
                 g.V(president).as("a").V(team).addE("possiede").from("a").next();
 
                 g.tx().commit();
@@ -191,9 +209,25 @@ public class FantaGraphBuild {
     }
 
     private static void buildCompositeIndex(JanusGraphManagement mgmt) {
-        PropertyKey citta = mgmt.getPropertyKey("citta");
-        JanusGraphManagement.IndexBuilder indexBuilder = mgmt.buildIndex("StadiumByCity", Vertex.class).addKey(citta);
+        PropertyKey stadium_name = mgmt.getPropertyKey("nome stadio");
+        JanusGraphManagement.IndexBuilder indexBuilder = mgmt.buildIndex("StadiumByName", Vertex.class).addKey(stadium_name);
         indexBuilder.buildCompositeIndex();
+        PropertyKey team_name = mgmt.getPropertyKey("nome squadra");
+        indexBuilder = mgmt.buildIndex("TeamByName", Vertex.class).addKey(team_name);
+        indexBuilder.buildCompositeIndex();
+        PropertyKey player_id = mgmt.getPropertyKey("id");
+        indexBuilder = mgmt.buildIndex("PlayerById", Vertex.class).addKey(player_id);
+        indexBuilder.buildCompositeIndex();
+        PropertyKey coach_name = mgmt.getPropertyKey("nome allenatore");
+        indexBuilder = mgmt.buildIndex("CoachByName", Vertex.class).addKey(coach_name);
+        indexBuilder.buildCompositeIndex();
+        PropertyKey president_name = mgmt.getPropertyKey("nome presidente");
+        indexBuilder = mgmt.buildIndex("PresidentByName", Vertex.class).addKey(president_name);
+        indexBuilder.buildCompositeIndex();
+        PropertyKey prosecutor_name = mgmt.getPropertyKey("nome procuratore");
+        indexBuilder = mgmt.buildIndex("ProsecutorByName", Vertex.class).addKey(prosecutor_name);
+        indexBuilder.buildCompositeIndex();
+
     }
 
     public static void main(String[] args) throws Exception {
@@ -207,28 +241,14 @@ public class FantaGraphBuild {
         createSchema(management);
 
         GraphTraversalSource g = graph.traversal();
-        createTeamVertex(g);
-        createPlayerVertex(g);
+        createInfosVertices(g);
+        createPlayersVertices(g);
 
-        //search one president from team
-        //Map<Object, Object> presVertex = g.V().hasLabel("squadra").has("nome", "PARMA").in("possiede").valueMap().next();
-        //search one team from president passing through coach
-        double queryAll = System.currentTimeMillis();
-        Map<Object, Object> teamVertex = g.V().hasLabel("presidente").has("nome", "GIORGIO SQUINZI").out("ha_assunto").out("allena").valueMap().next();
-        double queryIndex = System.currentTimeMillis();
-        //check the ROMA stadium is shared
-        //List<Map<Object, Object>> stadium_fans = g.V().hasLabel("squadra").has("nome", "ROMA").out("gioca_in").in("gioca_in").valueMap().toList();
-        Object teams = g.V().has("citta", "ROMA").in("gioca_in").values("nome").toList();
-        Object players = g.V().hasLabel("squadra").has("nome", "FIORENTINA").in("gioca_per").values("nome").toList();
-        double endTime = System.currentTimeMillis();
+        //test if everything is ok
+        Object vertex = g.V().has("id", "2002").out("gioca per").in("possiede")
+                .in("è incaricato da").out("allena").out("gioca in").values("nome stadio").next();
 
-        //LOGGER.info(String.valueOf(queryIndex - queryAll));
-        //LOGGER.info(String.valueOf(endTime - queryIndex));
-        //LOGGER.info(presVertex.toString());
-        LOGGER.info(teamVertex.toString());
-        //LOGGER.info(stadium_fans.toString());
-        LOGGER.info(teams.toString());
-        LOGGER.info(players.toString());
+        LOGGER.info("TEST: LO STADIO DOVREBBE ESSERE ARTEMIO FRANCHI E RISULTA " + vertex);
         System.exit(0);
     }
 }
